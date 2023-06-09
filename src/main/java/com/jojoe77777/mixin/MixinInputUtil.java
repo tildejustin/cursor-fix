fcd9be6103ea7a1e91310103fa6f2e3d33f20a10
jojoe77777 <jojoe77777@users.noreply.github.com>
2023-06-09 20:28:21 +1000

    Initialn't commit

---
+++ src/main/java/com/jojoe77777/mixin/MixinInputUtil.java
@@ -0,0 +1,21 @@
+package com.jojoe77777.mixin;
+
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(InputUtil.class)
+public class MixinInputUtil {
+
+    /**
+     * @author jojoe77777
+     * @reason Swapping the order of these lines fixes cursor position not always updating
+     */
+    @Overwrite
+    public static void setCursorParameters(long handler, int i, double d, double e) {
+        GLFW.glfwSetInputMode(handler, 208897, i);
+        GLFW.glfwSetCursorPos(handler, d, e);
+    }
+
+}
