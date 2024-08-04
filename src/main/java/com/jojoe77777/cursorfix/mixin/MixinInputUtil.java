686f0998a2dd19ea7f8f448f3949158c1fb75877
tildejustin <spacepepper3.14@gmail.com>
2024-08-03 21:35:40 -0400

    potential fix for random cursor offset instead of intentional cursor manipulation

--- src/main/java/com/jojoe77777/cursorfix/mixin/MixinInputUtil.java
+++ src/main/java/com/jojoe77777/cursorfix/mixin/MixinInputUtil.java
@@ -1,21 +1,15 @@
 package com.jojoe77777.cursorfix.mixin;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(InputUtil.class)
 public class MixinInputUtil {
-
-    /**
-     * @author jojoe77777
-     * @reason Swapping the order of these lines fixes cursor position not always updating
-     */
-    @Overwrite
-    public static void setCursorParameters(long handler, int i, double d, double e) {
-        GLFW.glfwSetInputMode(handler, 208897, i);
-        GLFW.glfwSetCursorPos(handler, d, e);
+    @WrapWithCondition(method = "setCursorParameters", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwSetCursorPos(JDD)V"))
+    private static boolean doNotSetCursorWhenClosingScreen(long window, double x, double y, long window_, int mode, double x_, double y_) {
+        return mode != GLFW.GLFW_CURSOR_NORMAL;
     }
-
 }
