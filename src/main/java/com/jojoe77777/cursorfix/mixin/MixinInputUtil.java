3a5e2e5d492117997075da10f73202b67e0c5d53
tildejustin <spacepepper3.14@gmail.com>
2025-03-27 23:42:58 -0400

    test repeating setCursorPos call setCursorPos is said to be occasionally ignored by windows, this is supposed to help https://github.com/libsdl-org/SDL/issues/4339#issuecomment-968080201 https://github.com/libsdl-org/SDL/issues/4165#issuecomment-944940673 https://forum.level1techs.com/t/setcursorpos-apparent-intermittent-behavior-solved-with-a-hammer-is-there-a-better-way/192003

--- src/main/java/com/jojoe77777/cursorfix/mixin/MixinInputUtil.java
+++ src/main/java/com/jojoe77777/cursorfix/mixin/MixinInputUtil.java
@@ -1,15 +1,16 @@
 package com.jojoe77777.cursorfix.mixin;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.*;
 import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(InputUtil.class)
 public class MixinInputUtil {
-//    @WrapWithCondition(method = "setCursorParameters", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwSetCursorPos(JDD)V"))
-//    private static boolean doNotSetCursorWhenClosingScreen(long window, double x, double y, long window_, int mode, double x_, double y_) {
-//        return mode != GLFW.GLFW_CURSOR_NORMAL;
-//    }
+    @WrapOperation(method = "setCursorParameters", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwSetCursorPos(JDD)V"))
+    private static void repeatSetCursorPos(long window, double xpos, double ypos, Operation<Void> original) {
+        original.call(window, xpos, ypos);
+        original.call(window, xpos + 1, ypos);
+        original.call(window, xpos, ypos);
+    }
 }
